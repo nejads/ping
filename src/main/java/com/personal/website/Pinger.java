363fb5e7c6d050677280f99f2e6a5d245cfa8f37
Soroush Nejad <soroush.nejad@kits.se>
2017-01-21 21:23:15 +0100

    Better 5

--- src/main/java/com/personal/website/Pinger.java
+++ src/main/java/com/personal/website/Pinger.java
@@ -15,21 +15,29 @@ public class Pinger
     @Scheduled(cron="*/20 * * * * ?")
     public void ping()
     {
-        System.out.println("Pinging is started. Current time is :: "+ new Date());
+        System.out.println("Pinging Openshift is started. Current time is :: "+ new Date());
 
         HttpClient client = new HttpClient();
         HttpMethod method_openshift = new GetMethod("http://soroush-webbsidan.rhcloud.com/");
-        HttpMethod method_heroku = new GetMethod("https://pingeropenshift.herokuapp.com/");
         try {
             client.executeMethod(method_openshift);
-            // client.executeMethod(method_heroku);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Pinging finished successfully. Server: " +
+        System.out.println("Pinging Openshift finished successfully. Server: " +
                 method_openshift.getResponseHeader("Server").getValue());
 
+
+        System.out.println("Pinging Heroku is started. Current time is :: "+ new Date());
+        HttpMethod method_heroku = new GetMethod("https://pingeropenshift.herokuapp.com/");
+        try {
+            client.executeMethod(method_heroku);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Pinging Heroku finished successfully. Server: " +
+                method_heroku.getResponseHeader("Server").getValue());
     }
 
 }
