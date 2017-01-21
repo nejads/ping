a44958d1f028aa1592d5bfce91b096460ca8fe9c
Soroush Nejad <soroush.nejad@kits.se>
2017-01-21 20:56:34 +0100

    Better pinger4

--- src/main/java/com/personal/website/PingerApplication.java
+++ src/main/java/com/personal/website/PingerApplication.java
@@ -6,18 +6,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @ComponentScan(basePackageClasses = AppConfig.class)
 @ComponentScan(basePackageClasses = Pinger.class)
 @SpringBootApplication
 public class PingerApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
 
 	// web: java -jar target/pinger-0.0.1-SNAPSHOT.jar & python pinger.py
 	public static void main(String[] args) {
